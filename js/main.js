20445bad00d79ef4a4b314bf226423eb9fd7b4e9
5oN1A <guarino.sonia@gmail.com>
2021-10-12 16:26:43 +0200

    if setting

--- js/main.js
+++ js/main.js
@@ -1,25 +1,41 @@
 /*
 1.creo una row in html per poter assegnare una col ad ogni risultato dell'iterazione
 2.creo ciclo da 1 a 100 con incremento di +1;
-3.se non multiplo di 3 e non multiplo di 5 = bg-color #1389b2 + p ${i}
-se multiplo di 3 ma non multiplo di 5 = bg-color #10D6A1 + p fizz
-se non multiplo di 3 ma multiplo di 5 =  bg-color #FFD166 + p buzz
-se multiplo di 3 e multiplo di 5 =  bg-color #ED456F + p fizzbuzz
+3.assegno una col ad ogni risultato dell'iterazione
+4.inserisco if all'interno del ciclo con i valori:
+    se non multiplo di 3 e non multiplo di 5 = bg-color #1389b2 + p ${i}
+    se multiplo di 3 ma non multiplo di 5 = bg-color #10D6A1 + p fizz
+    se non multiplo di 3 ma multiplo di 5 =  bg-color #FFD166 + p buzz
+    se multiplo di 3 e multiplo di 5 =  bg-color #ED456F + p fizzbuzz
 */
-
 
 const cellsContainer = document.getElementById("cells-container");
 
+
 //1.creo ciclo da 1 a 100 con incremento di +1;
 
-for (let i = 1; i <=100; i++) {
-
+for (let i = 1; i <= 100; i++) {
     const cell = document.createElement("div");
-    
-    cell.classList.add("col-2");
-    
-    cell.innerHTML += `<div class="ratio ratio-1x1 border">
-                            <div>${i}</div>
+
+    cell.classList.add("my-col-7");
+    cell.innerHTML += `<div class="ratio d-flex justify-content-center align-items-center ratio-1x1 my-bg-blue">
+                            ${i}
                         </div>`
     cellsContainer.append(cell);
+
+
+
+
+   /* if (i % 3 === 0) && (i % 5 != 0) {  
+       
+     }else if (i % 5 === 0) && (i % 3 != 0) {
+
+     }else if (i % 3 === 0) && (i % 5 === 0) {
+
+     } else {
+         
+     }
+     
+     
+     */
 }
